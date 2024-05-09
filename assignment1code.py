11e9c47aa6545a6f8deced636bedd7544d1864bf
NathanDO <122122847+nathanbrz@users.noreply.github.com>
2024-05-09 08:22:53 -0600

    Edit/Update code as required.

--- assignment1code.py
+++ assignment1code.py
@@ -1,10 +1,12 @@
 # ENSF 692 Spring 2024
 # Assignment 1 Starter Code
-# Add your name here!
+# Author= Nathan De Oliveira
 
-studentName = "Sarah Shah"  # Change the contents of this variable to your name (leave the quotation marks).
+studentName = "Nathan De Oliveira"  # Change the contents of this variable to your name (leave the quotation marks).
 
 print("Hello, World!")
 print("My name is " + studentName + " and I am a student in ENSF 692.")
 
 # print your best skill here
+print("My best skills dancing and time management (organization).")
+
